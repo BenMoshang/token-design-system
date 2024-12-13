bdcd6a274014db01618d486918af130431104644
Ben Moshang <benmoshang@gmail.com>
2024-12-13 14:05:37 -0500

    Refactor Vite configuration and SCSS structure; remove unused files and enhance app metadata
    - Deleted the old vite.config.js and replaced it with vite.config.ts for improved TypeScript support.
    - Removed unused SCSS files and font assets to streamline the project.
    - Updated app.html with enhanced metadata for better SEO and user engagement.
    - Added new font-face declarations for Roboto and Inter in layout styles.
    - Cleaned up layout styles by removing unnecessary gaps and commented-out code.

---
+++ src/lib/scss/scss-structure.md
@@ -0,0 +1,9 @@
+/scss
+|-- abstracts/     // Variables, functions, mixins, placeholders
+|-- base/          // Reset, typography, generic styles
+|-- components/    // Buttons, cards, modals, etc.
+|-- layout/        // Grid, header, footer, nav, sections
+|-- pages/         // Specific page styles (e.g., home.scss)
+|-- themes/        // Theme-specific styles
+|-- vendors/       // External libraries or frameworks
+|-- main.scss      // Main entry file importing everything
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import Footer from '../lib/Layout/Footer.svelte';
 	import Header from '../lib/Layout/Header.svelte';
 
-
 </script>
 
 <!-- <Header /> -->
@@ -13,7 +12,6 @@
 
 
 <style lang="scss">
-
 	.layout__main {
 		display: flex;
 		flex-direction: column;
@@ -27,6 +25,50 @@
 
 	}
 
+/* Roboto */
+@font-face {
+  font-family: 'Roboto';
+  src: 
+       url('/static/Fonts/Roboto-Regular.ttf') format('truetype');
+  font-weight: 400;
+  font-display: swap;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Roboto';
+  src: 
+       url('/static/Fonts/Roboto-Medium.ttf') format('truetype');
+  font-weight: 500;
+  font-display: swap;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Roboto';
+  src: 
+       url('/static/Fonts/Roboto-Bold.ttf') format('truetype');
+  font-weight: 600;
+  font-display: swap;
+  font-style: normal;
+}
+
+
+@font-face {
+  font-family: 'Inter';
+  src: url('/static/Fonts/Inter-VariableFont_opsz\,wght.ttf') format('truetype');
+  font-weight: 400;
+  font-display: swap;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Inter';
+  src: url('/static/Fonts/Inter-Italic-VariableFont_opsz\,wght.ttf') format('truetype');
+  font-weight: 400;
+  font-display: swap;
+  font-style: italic;
+}
 
 </style>
 	
