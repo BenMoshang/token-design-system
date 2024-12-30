92b0d0da5a52378573412df0bac70c1a97ce2a6c
Ben Moshang <benmoshang@gmail.com>
2024-12-29 20:16:46 -0500

    s

--- src/lib/Testing/FontTesting.svelte
+++ src/lib/Testing/FontTesting.svelte
@@ -1,46 +1,82 @@
-<h1>Large Display</h1>
-<h2>Medium Display</h2>
-<h3>Small Display</h3>
-<h4>Large Heading</h4>
-<h5>Medium Heading</h5>
-<h6>Small Heading</h6>
-<p class="p--base">Body Base</p>
-<p class="p--sm">Body Small</p>
-<p class="p--xs">Body Xtra Small</p>
+<section>
+	<h1 class="responsive">responsive</h1>
+	<h1 class="static">static</h1>
+	<h1 class="static--2">static--2</h1>
+
+	<h1 class="display--xl">XL Display</h1>
+	<h1 class="display--lg">Large Display</h1>
+	<h1 class="display--md">Medium Display</h1>
+	<h1 class="display--sm">Small Display</h1>
+
+	<h2 class="heading--xl">XL Heading</h2>
+	<h2 class="heading--lg">Large Heading</h2>
+	<h3 class="heading--md">Medium Heading</h3>
+	<h4 class="heading--sm">Small Heading</h4>
+	<h5 class="heading--xs">Xtra Small Heading</h5>
+	<p class="body--xl">XL Body</p>
+	<p class="body--lg">Body lg</p>
+	<p class="body--md">Body md</p>
+	<p class="body--sm">Body Small</p>
+	<p class="body--xs">Body Xtra Small</p>
+</section>
 
 <style lang="scss">
-	h1 {
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 0.025rem;
+	}
+	.responsive {
+		font-size: get-res-fsz('display', 'sm', 'xl');
+	}
+	.static {
+		font-size: get-static-fsz('display', 'xl');
+	}
+	.static--2 {
+		font-size: get-static-fsz('display', 'sm');
+	}
+	.display--xl {
+		@extend %global__display--xl;
+	}
+	.display--lg {
 		@extend %global__display--lg;
 	}
-
-	h2 {
+	.display--md {
 		@extend %global__display--md;
 	}
-
-	h3 {
+	.display--sm {
 		@extend %global__display--sm;
 	}
+	.display--xs {
+		@extend %global__display--xs;
+	}
 
-	h4 {
+	.heading--xl {
+		@extend %global__heading--xl;
+	}
+	.heading--lg {
 		@extend %global__heading--lg;
 	}
-
-	h5 {
+	.heading--md {
 		@extend %global__heading--md;
 	}
-
-	h6 {
+	.heading--sm {
 		@extend %global__heading--sm;
 	}
-	.p--base {
-		@extend %global__body--base;
+	.heading--xs {
+		@extend %global__heading--xs;
 	}
 
-	.p--sm {
+	.body--xl {
+		@extend %global__body--xl;
+	}
+	.body--lg {
+		@extend %global__body--lg;
+	}
+	.body--md {
+		@extend %global__body--md;
+	}
+	.body--sm {
 		@extend %global__body--sm;
-	}
-
-	.p--xs {
-		@extend %global__body--xs;
 	}
 </style>
