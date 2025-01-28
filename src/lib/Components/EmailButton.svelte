0d784fcaf70444e1f6dacb00f8ceaf08035bbc21
Ben Moshang <benmoshang@gmail.com>
2025-01-27 20:22:04 -0500

    d

--- src/lib/Components/EmailButton.svelte
+++ src/lib/Components/EmailButton.svelte
@@ -20,6 +20,18 @@
 	// 		target.click();
 	// 	}
 	// }
+
+	let isHovered = $state(false);
+
+	function handleHover() {
+		// console.log('hover');
+		isHovered = true;
+	}
+
+	function handleLeave() {
+		// console.log('leave');
+		isHovered = false;
+	}
 </script>
 
 <!-- onclick={handleClick} -->
@@ -35,6 +47,8 @@
 	itemscope
 	itemtype="http://schema.org/ContactPoint"
 	itemprop="email"
+	onmouseenter={handleHover}
+	onmouseleave={handleLeave}
 >
 	<meta itemprop="contactType" content="customer service" />
 	<svg
@@ -54,18 +68,21 @@
 			d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
 		/>
 	</svg>
-	<span class="text" itemprop="name">{label}</span>
+	<span class:is-hovered={isHovered} class="text" itemprop="name">{label}</span>
 </a>
 
 <style lang="scss">
 	.email-button {
-		@include hover-brightness;
-		@include flex-center;
-		text-decoration: none;
+		@include shadow-low;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: get-sp('x2');
 		position: relative;
-		max-block-size: 2rem;
-		gap: get-sp('x1');
-		padding: get-sp('x2') get-sp('x3');
+		width: fit-content;
+		height: fit-content;
+		text-decoration: none;
+		padding: get-sp('x2');
 
 		// Typography styles
 		font-family: get-ff('primary');
@@ -78,7 +95,7 @@
 		// Border and shape
 		border: none;
 		border-radius: $br-rounded;
-
+		overflow: clip;
 		// Interactive states
 		cursor: pointer;
 		transition:
@@ -87,14 +104,28 @@
 
 		// Focus styles for accessibility
 		&:focus-visible {
-			outline: 3px solid get-light-dark('lightest', 'darkest');
-			outline-offset: 2px;
+			outline: 0.1875rem solid get-light-dark('lightest', 'darkest');
+			outline-offset: 0.125rem;
+		}
+
+		&:hover {
+			& span {
+				position: relative;
+			}
+		}
+
+		& span {
+			position: absolute;
+			transition-property: position;
+			transition-duration: 0.3s;
+			transition-timing-function: ease-in-out;
+			transition-behavior: allow-discrete;
+			text-wrap: nowrap;
 		}
 
 		& svg {
-			display: inline flex;
-			inline-size: 0.75rem;
-			block-size: 0.75rem;
+			inline-size: 1.125rem;
+			block-size: 1.125rem;
 			vertical-align: bottom;
 
 			& path {
@@ -102,7 +133,14 @@
 			}
 		}
 	}
-
+	@keyframes show-text {
+		from {
+			display: none;
+		}
+		to {
+			display: block;
+		}
+	}
 	.text {
 		font-weight: get-fw('medium');
 	}
